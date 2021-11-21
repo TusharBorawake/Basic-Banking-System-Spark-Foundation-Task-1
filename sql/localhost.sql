4e0084d2454030a242fb919c51fe23df1bac376a
TusharBorawake <94785765+TusharBorawake@users.noreply.github.com>
2021-11-21 20:57:30 +0530

    Update localhost.sql

--- sql/localhost.sql
+++ sql/localhost.sql
@@ -1,34 +1,24 @@
--- phpMyAdmin SQL Dump
--- version 4.9.5
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Apr 09, 2021 at 05:56 AM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.3.23
+**************************************************************************************
+Step 1: Open xampp server pannel
+Step 2: Start Apache and MySQL
+Step 3: Click on Admin Button next to MySQL to start phpmyadmin
+Step 4: Click on databases and create database with any name (In my case "01_Tushar")
+Step 5: Then select that database and click on SQL 
+Step 4: Run all this command in that SQL tab one by one
+Step 5: Your backend part is ready
+**************************************************************************************
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+---------------------------------- Creating Database -----------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `id16557745_anisha`
---
 CREATE DATABASE IF NOT EXISTS `01_Tushar` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `01_Tushar`;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `transaction`
---
+----------------------------------------- Table structure for table `transaction` ------------------------------------------------
 
 CREATE TABLE `transaction` (
   `sno` int(3) NOT NULL,
@@ -38,20 +28,18 @@ CREATE TABLE `transaction` (
   `datetime` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `transaction`
---
+
+----------------------------- Inserting dummy data into table `transaction` ---------------------------------
+
 
 INSERT INTO `transaction` (`sno`, `sender`, `receiver`, `balance`) VALUES
 (2, 'Tushar', 'Pratik', 9),
 (3, 'Rahul', 'Ganesh', 100),
 (4, 'Ganesh', 'Omkar', 1);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `users`
---
+------------------------------------------ Table structure for table `users` ----------------------------------------------------
+
 
 CREATE TABLE `users` (
   `id` int(3) NOT NULL,
@@ -60,9 +48,9 @@ CREATE TABLE `users` (
   `balance` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
+
+--------------------------------------- Inserting dummy data into table `users` -----------------------------------------------------
+
 
 INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
 (1, 'Tushar', 'tushar@gmail.com', 100000),
@@ -71,39 +59,25 @@ INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
 (4, 'Ganesh', 'ganesh@gmail.com', 1000000),
 (5, 'Rohan', 'rohan@gmail.com', 4000000);
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `transaction`
---
 ALTER TABLE `transaction`
   ADD PRIMARY KEY (`sno`);
 
---
--- Indexes for table `users`
---
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `transaction`
---
+------------------------------------------ AUTO_INCREMENT for table `transaction` -----------------------------------------------------------
+
+
 ALTER TABLE `transaction`
   MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT for table `users`
---
+
+------------------------------------------------- AUTO_INCREMENT for table `users` ------------------------------------------------------------
+
+
 ALTER TABLE `users`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
