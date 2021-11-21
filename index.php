003c5b40d588509151435bc918d084cb06715604
TusharBorawake <94785765+TusharBorawake@users.noreply.github.com>
2021-11-21 19:12:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Basic Banking System</title>
+  <style>
+footer 
+{
+   display: flex;
+   background-color: #f8f9fa;
+   overflow: hidden;
+}
+
+.footer:hover 
+{
+   color: red;
+   background-color: rgb(216, 231, 4);
+}
+
+footer a {
+   text-decoration: none;
+   padding: 14px;
+   text-align: center;
+   float: left;
+   color: Black;
+   font-size: 16px;
+}
+	</style>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      <div class="container-fluid">
+      <!-- Introduction section -->
+            <div >
+            <div>
+                <div class="heading text-center my-4">
+                  <h3 style=" color:White ; background-image: linear-gradient(#ffcc00, #ff2c00); border-radius: 12px;">Welcome Customer's</h3>
+                  <h1 style=" color:#403ce3 ; background-image: linear-gradient(#00ffa5, yellow); border-radius: 12px;">TPB Bank of India</h1>
+                </div>
+              </div>
+            </div>
+			<div class="container-fluid" >
+			<div class="container-fluid-sm" >
+               <img src="image/Bank1.png" style=" height: 251px; margin-left: 252px;">
+     </div></div>
+      </div>
+      <footer>
+        <a href="#" class="footer">FAQ</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="#" class="footer">Contact Us</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="#" class="footer">Terms and Conditions</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="#" class="footer">Created by Tushar Borawake | &copy; copyright 2021 All rights reserved</a>
+    </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+ </body>
+</html>
